3552acd53836538e74c76eb286ad40550d12b615
JeevanSadalge <jeevanhrx@gmail.com>
2021-08-02 09:37:39 +0530

    section-6 asynchronous node js finished

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const geoCode = require("./utils/geocode");
+const foreCast = require("./utils/forecast");
+const add = process.argv[2];
+
+geoCode(add, (err, { location, longitude, lattitude } = {}) => {
+  if (!add) console.log("please provide a address");
+  else {
+    if (err) console.log(err);
+    else {
+      // const  = data;
+      foreCast(longitude, lattitude, (error, forecastData) => {
+        if (error) console.log(error);
+        else {
+          console.log("You are currently seeing weather for", location);
+          console.log(forecastData);
+        }
+      });
+    }
+  }
+});
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require("request");
+function foreCast(longitude, lattitude, callback) {
+  const url = `http://api.weatherstack.com/current?access_key=d272af71a658407b3a618bd116dfe2f3&query=${lattitude},${longitude}&units=s`;
+  request({ url, json: true }, (err, response, body) => {
+    if (err) callback("Unable to connect to weather service.", undefined);
+    else if (body.error) {
+      callback(body.error.info, undefined);
+    } else {
+      // console.log("status code:-", response.statusCode);
+      callback(undefined, {
+        weather_descriptions: body.current.weather_descriptions[0],
+        temperature: body.current.temperature,
+        feelsLike: body.current.feelslike,
+      });
+    }
+  });
+}
+module.exports = foreCast;
